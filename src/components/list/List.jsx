16da9e758db13ce95983dcdea1bc5f5776d8d73b
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-09-06 19:21:43 +0200

    Added Loader component and logic in List component regarding fetching data, Loader.css

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 import ListItem from "./ListItem.jsx";
+import Loader from "./Loader.jsx";
 
-export default function List({ location }) {
+export default function List(props) {
   const [weatherData, setWeatherData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
+    let location = Array.isArray(props.location) ? props.location.join(",") : props.location;
+
     fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${location}&days=7&aqi=no&alerts=no`
     )
       .then((res) => res.json())
-      .then((data) => setWeatherData(data.forecast.forecastday));
-  }, [location]);
+      .then((data) => setWeatherData(data.forecast.forecastday))
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false));
+
+  }, [props.location]);
+
   return (
+    <>
+    {isLoading && <Loader />}
     <ul className="list">
       {weatherData.map((item, i) => (
         <ListItem weatherItem={item} key={i} />
       ))}
     </ul>
+    </>
   );
 }
