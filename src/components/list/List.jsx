2cb12ce7d98245ce89e240ed24c5318adb62862c
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-09-08 18:18:07 +0200

    changed from promise chaining handling of fetch in effect to async/await

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,18 +6,21 @@ import Loader from "./Loader.jsx";
 export default function List(props) {
   const [weatherData, setWeatherData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    let [lat, lon] = props.location;
+    (async () => {
 
-    fetch(
-        `https://api.weatherbit.io/v2.0/current?key=${process.env.NEW_API_KEY}&lang=${props.userLanguage}&lat=${lat}&lon=${lon}&days=7`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-        return setWeatherData(data.forecast.forecastday)})
-      .catch(err => console.log(err))
-      .finally(() => setIsLoading(false));
+        const [lat, lon] = props.location;
+
+        try {
+        const res = await fetch(`https://api.weatherbit.io/v2.0/current?key=${process.env.NEW_API_KEY}&lang=${props.userLanguage}&lat=${lat}&lon=${lon}&days=7`)
+        const data = await res.json();
+        setWeatherData();
+        } catch(err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    })();
 
   }, [props.location, props.userLanguage]);
 
