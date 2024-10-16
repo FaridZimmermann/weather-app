dfb609ef13f3ebc9fb4c07a47725e874532b0128
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-10-16 13:45:34 +0200

    Removed overlay for Card View, code cleanup

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,7 +1,4 @@
-import { useState, useContext } from "react";
-
-
-import ListItemCard from "./ListItemCard.jsx";
+import {useContext } from "react";
 
 import { SettingsContext } from "../../contexts/SettingsContext";
 import { tempSymb } from "../../helpers/valueMaps";
@@ -9,12 +6,10 @@ import { tempSymb } from "../../helpers/valueMaps";
 import "./ListItem.css";
 
 export default function ListItem({ weatherItem }) {
-  const [cardOpen, setCardOpen] = useState(false);
   const settings = useContext(SettingsContext);
 
   return (
     <>
-    {cardOpen && <ListItemCard item={weatherItem} setCardOpen={setCardOpen}/>}
     <li className="list-item" onClick={() => setCardOpen(true)}>
       <h3>{weatherItem.dt_txt.split(" ")[0].split("-").reverse().join("-")}</h3>
       <img src={`https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png`} />
