ddc1322d690e5997ae66275917654d05d195b758
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-10-16 13:36:44 +0200

    Added Temperature to ListItem

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 
 import ListItemCard from "./ListItemCard.jsx";
 
+import { SettingsContext } from "../../contexts/SettingsContext";
+import { tempSymb } from "../../helpers/valueMaps";
 
 import "./ListItem.css";
 
 export default function ListItem({ weatherItem }) {
   const [cardOpen, setCardOpen] = useState(false);
+  const settings = useContext(SettingsContext);
 
   return (
     <>
@@ -16,7 +19,7 @@ export default function ListItem({ weatherItem }) {
       <h3>{weatherItem.dt_txt.split(" ")[0].split("-").reverse().join("-")}</h3>
       <img src={`https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png`} />
       <p>{weatherItem.weather[0].description}</p>
-
+      <p>{`${Math.floor(weatherItem.main.temp)}${tempSymb[settings.tempUnit]}`}</p>
     </li>
     </>
   );
